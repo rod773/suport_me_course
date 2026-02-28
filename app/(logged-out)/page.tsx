c735063510ccd84d3f360aefbdbcabb4d271229f
rod773 <rodrigoarcadio1@gmail.com>
2026-02-28 21:56:22 +0100

    pink color icon

--- app/(logged-out)/page.tsx
+++ app/(logged-out)/page.tsx
@@ -5,7 +5,7 @@ const LandingPage = () => {
   return (
     <>
       <h1 className="flex gap-2 items-center">
-        <PersonStandingIcon size={50} />
+        <PersonStandingIcon size={50} className="text-pink-500" />
         Support Me
       </h1>
       <Button>Log In</Button>
