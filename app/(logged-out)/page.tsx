763d61649d97e742c22521b4bdf9759e50a8cefa
rod773 <rodrigoarcadio1@gmail.com>
2026-02-28 18:57:43 +0100

    buttons

---
+++ app/(logged-out)/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+const LandingPage = () => {
+  return (
+    <div>
+      <h1>Spport Me</h1>
+      <Button>Log In</Button>
+      <Button>Sign In</Button>
+    </div>
+  );
+};
+
+export default LandingPage;
